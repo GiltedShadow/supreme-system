b8e92412e2b20a99ef4e0e5df70296b17f5fe331
Alex Norment <87099499+GiltedShadow@users.noreply.github.com>
2024-04-17 15:59:37 -0700

    update DND5e

--- CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/MainMenuActivity.java
+++ CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/MainMenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.ceis320_finalapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,5 +28,42 @@ Dice pages - Percentile, 100, 50, 20, 12, 8, 6, 4, 3, 2
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnInfo=(Button) findViewById(R.id.btnDND5);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { goDND5(); }
+        });
+
+        Button btnTakePic=(Button) findViewById(R.id.btnGeneralRoll);
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goGeneralRoll();
+            }
+        });
+
+        Button btnFavSong=(Button) findViewById(R.id.btnPathFinder);
+        btnFavSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goPathFinder();
+            }
+        });
+    }
+
+    private void goDND5() {
+        Intent intent = new Intent(MainMenuActivity.this, FifthEditionActivity.class);
+        this.startActivities(new Intent[]{intent});
+    }
+
+    private void goGeneralRoll() {
+        Intent intent = new Intent(MainMenuActivity.this, GeneralRollerActivity.class);
+        this.startActivities(new Intent[]{intent});
+    }
+
+    private void goPathFinder() {
+        Intent intent = new Intent(MainMenuActivity.this, PathfinderActivity.class);
+        this.startActivities(new Intent[]{intent});
     }
 }
