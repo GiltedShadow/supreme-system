b2cdf4db5befff3e4e319ce98f5bdfccb5d0f159
Alex Norment <87099499+GiltedShadow@users.noreply.github.com>
2024-04-16 21:59:53 -0500

    Start final project
    create all activities
    start on main menu

--- CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/FifthEditionActivity.java
+++ CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/FifthEditionActivity.java
@@ -8,18 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-/*
-TODO
-Selection pages -  Dnd 5e, Pathfinder, general
-Dice pages - Percentile, 100, 50, 20, 12, 8, 6, 4, 3, 2
+public class FifthEditionActivity extends AppCompatActivity {
 
- */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fifth_edition);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/MainMenuActivity.java
@@ -0,0 +1,29 @@
+package com.example.ceis320_finalapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainMenuActivity extends AppCompatActivity {
+/*
+TODO
+Selection pages -  Dnd 5e, Pathfinder, general
+Dice pages - Percentile, 100, 50, 20, 12, 8, 6, 4, 3, 2
+
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
