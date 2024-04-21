df2176976a152057865d0bc0470159ab7938fccb
Alex Norment <87099499+GiltedShadow@users.noreply.github.com>
2024-04-21 00:21:51 -0500

    Final project turn in
    major app functions 100%

--- CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/MultipleGeneralRollerActivity.java
+++ CEIS320_FinalApplication/app/src/main/java/com/example/ceis320_finalapplication/MultipleGeneralRollerActivity.java
@@ -1,9 +1,13 @@
 package com.example.ceis320_finalapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MultipleGeneralRollerActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MultipleGeneralRollerActivity extends AppCompatActivity implements View.OnClickListener/*, AdapterView.OnItemClickListener*/ {
+    Spinner spnDice, spnNumber;
+    Button btnRoll;
+    TextView lblHighOutput, lblLowOutput, lblAvgOutput, lblTotalOutput, lblResultOutput, lblMultGenCustNum;
+    ArrayList<String> resultOfRolls = new ArrayList<>();
+    EditText txtCustomInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,106 +35,128 @@ public class MultipleGeneralRollerActivity extends AppCompatActivity {
             return insets;
         });
 
-        //Commented out code here is a previous design that used multiple buttons to go to each individual page
-        /*Button btnMultiD2=(Button) findViewById(R.id.btnMultGenD2);
-        btnMultiD2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD2(); }
-        });
-        Button btnMultiD3=(Button) findViewById(R.id.btnMultGenD3);
-        btnMultiD3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD3(); }
-        });
-        Button btnMultiD4=(Button) findViewById(R.id.btnMultGenD4);
-        btnMultiD4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD4(); }
-        });
-        Button btnMultiD6=(Button) findViewById(R.id.btnMultGenD6);
-        btnMultiD6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD6(); }
-        });
-        Button btnMultiD8=(Button) findViewById(R.id.btnMultGenD8);
-        btnMultiD8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD8(); }
-        });
-        Button btnMultiD10=(Button) findViewById(R.id.btnMultGenD10);
-        btnMultiD10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD10(); }
-        });
-        Button btnMultiD212=(Button) findViewById(R.id.btnMultGenD12);
-        btnMultiD212.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD12(); }
-        });
-        Button btnMultiD20=(Button) findViewById(R.id.btnMultGenD20);
-        btnMultiD20.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD20(); }
-        });
-        Button btnMultiD50=(Button) findViewById(R.id.btnMultGenD50);
-        btnMultiD50.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD50(); }
-        });
-        Button btnMultiD100=(Button) findViewById(R.id.btnMultGenD100);
-        btnMultiD100.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollD100(); }
-        });
-        Button btnMultiPercentile=(Button) findViewById(R.id.btnMultGenPercentile);
-        btnMultiPercentile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { goMultiRollPercentile(); }
-        });*/
+        spnDice = (Spinner) findViewById(R.id.spnMultGenTypeOfDice);
+        spnNumber = (Spinner) findViewById(R.id.spnMultGenAmountOfDice);
+        btnRoll = (Button) findViewById(R.id.btnMultGenRollDie);
+        txtCustomInput = (EditText) findViewById(R.id.txtMultGenCustomInput);
+        lblHighOutput = (TextView) findViewById(R.id.lblMultGenHigh);
+        lblLowOutput = (TextView) findViewById(R.id.lblMultGenLow);
+        lblAvgOutput = (TextView) findViewById(R.id.lblMultGenAvg);
+        lblTotalOutput = (TextView) findViewById(R.id.lblMultGenTotal);
+        lblResultOutput = (TextView) findViewById(R.id.lblMultGenResult);
+        lblMultGenCustNum = (TextView) findViewById(R.id.lblMultGenCustNum);
+        lblResultOutput.setText("");
+        /*//Currently unable to do this, app crashes immediately
+        txtCustomInput.setVisibility(View.INVISIBLE);
+        lblMultGenCustNum.setVisibility(View.INVISIBLE);*/
+
+        btnRoll.setOnClickListener(this);
+
+
     }
 
-    /*private void goMultiRollD2() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, TwoSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD3() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, ThreeSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD4() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, FourSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD6() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, SixSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD8() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, EightSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD10() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, TenSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD12() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, TwelveSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD20() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, TwentySideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD50() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, FiftySideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollD100() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, OneHundredSideRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
-    }
-    private void goMultiRollPercentile() {
-        Intent intent = new Intent(MultipleGeneralRollerActivity.this, PercentileRollerActivity.class);
-        this.startActivities(new Intent[]{intent});
+
+    @Override
+    public void onClick(View v) {
+        String resultToOutput = "";
+        int lowestRolled = 100;
+        int highestRolled = 0;
+        int numberRolled;
+        int totalRolled = 0;
+        int numberToRoll = 0;
+        int dieSidesNeeded = 1;
+        System.out.println(spnDice.getSelectedItem());
+        switch (spnDice.getSelectedItemPosition()){
+            case 0:
+                dieSidesNeeded = 2;
+                break;
+            case 1:
+                dieSidesNeeded = 3;
+                break;
+            case 2:
+                dieSidesNeeded = 4;
+                break;
+            case 3:
+                dieSidesNeeded = 6;
+                break;
+            case 4:
+                dieSidesNeeded = 8;
+                break;
+            case 5:
+                dieSidesNeeded = 10;
+                break;
+            case 6:
+                dieSidesNeeded = 12;
+                break;
+            case 7:
+                dieSidesNeeded = 20;
+                break;
+            case 8:
+                dieSidesNeeded = 50;
+                break;
+            case 9:
+                dieSidesNeeded = 100;
+                break;
+            case 10:
+                dieSidesNeeded = 100;
+                break;
+            default:
+                dieSidesNeeded = 1;
+                break;
+        }
+        System.out.print("Die sides needed = ");
+        System.out.println(dieSidesNeeded);
+        ///Currently unable to do this, app crashes immediately
+        /*if (spnNumber.getSelectedItem().equals("Custom")) {
+            txtCustomInput.setVisibility(View.VISIBLE);
+            lblMultGenCustNum.setVisibility(View.VISIBLE);
+        } else {
+            txtCustomInput.setVisibility(View.INVISIBLE);
+            lblMultGenCustNum.setVisibility(View.INVISIBLE);
+        }*/
+
+        if (spnNumber.getSelectedItem().equals("Custom")) {
+            try {
+                numberToRoll = Integer.parseInt(txtCustomInput.getText().toString());
+                //System.out.println(numberToRoll);
+            } catch (Exception e) {
+                System.out.println(e);
+                Toast.makeText(MultipleGeneralRollerActivity.this, "Please enter a real number", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            try {
+                numberToRoll = Integer.parseInt(spnNumber.getSelectedItem().toString());
+                System.out.println(numberToRoll);
+            } catch (Exception e) {
+                System.out.println(e);
+                Toast.makeText(MultipleGeneralRollerActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+        for(int x=0;x<numberToRoll;x++){
+            numberRolled = Integer.parseInt(MainMenuActivity.getRandomNumber(dieSidesNeeded));
+            if(resultToOutput.equals("")){resultToOutput = String.valueOf(numberRolled);}
+            else{resultToOutput = String.format("%s, %s", resultToOutput, String.valueOf(numberRolled));}
+            resultOfRolls.add(String.valueOf(numberRolled));
+            totalRolled += numberRolled;
+            if(numberRolled>highestRolled){highestRolled=numberRolled;}
+            if(lowestRolled>numberRolled){lowestRolled=numberRolled;}
+            System.out.println(x);
+        }
+        System.out.println("success");
+        lblTotalOutput.setText(String.valueOf(totalRolled));
+        lblAvgOutput.setText(String.valueOf(totalRolled/numberToRoll));
+        lblHighOutput.setText(String.valueOf(highestRolled));
+        lblLowOutput.setText(String.valueOf(lowestRolled));
+        lblResultOutput.setText(resultToOutput);
+        System.out.println(totalRolled);
+        System.out.println(String.valueOf(totalRolled/numberToRoll));
+        System.out.println(String.valueOf(highestRolled));
+        System.out.println(String.valueOf(lowestRolled));
+        System.out.println(resultToOutput);
+
+    /*@Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }*/
+    }
 }
